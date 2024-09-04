5d47aa760a6bbffb1addc6183cee109cf40dec99
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-09-04 19:31:36 -0400

    added route to insert into contact table

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ passportConfig(passport);
 
 // routes for authetication
 const authRoutes = require('./routes/auth.js');
+const { result } = require('underscore');
 app.use('/auth', authRoutes);
 
 
@@ -119,6 +120,60 @@ app.get('/api/logout', (req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// contact form routes
+
+app.post('/api/submit-form', async (req, res) => {
+    const { firstName, lastName, email, subject, comment } = req.body;
+  
+    try {
+    
+      if (!firstName || !lastName || !email || !subject || !comment) {
+        return res.status(400).json({ error: 'All fields are required.' });
+      }
+  
+     
+      const result = await db.query(
+        'INSERT INTO Contact_Form (email, phone, first_name, last_name, comment) VALUES ($1, $2, $3, $4, $5)',
+        [email, null, firstName, lastName, comment]  ,
+        console.log(result)
+      );
+  
+      res.status(200).json({ message: 'Form submitted successfully.' });
+    } catch (error) {
+      console.error('Error inserting data:', error);
+      res.status(500).json({ error: 'Error submitting form.' });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+app.get('/contact/info', async (req, res) => {
+    try {
+        const [rows] = await db.execute('SELECT * FROM Contact_Form;');
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while fetching users.' });
+    }
+});
 
 
 
